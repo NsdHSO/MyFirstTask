b62a3f117a92e0dcf42d175346425c26c2c080f5
Sami David <HSO@192-168-0-104.rdsnet.ro>
2020-08-30 23:01:04 +0300

    Finally X and 0 Game No DataBase follows futher

--- app/src/main/java/com/example/task1/MainActivity.java
+++ app/src/main/java/com/example/task1/MainActivity.java
@@ -3,12 +3,183 @@ package com.example.task1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private Button[][] buttons = new Button[3][3];
+
+    private TextView scorePlayer1;
+    private TextView scorePlayer2;
+    private TextView equality;
+    private int playerPuncte1;
+    private int playerPuncte2;
+    private boolean firstRound = !true;
+    private int equalityNumber;
+    private int numberOfLaps;
+
+    /*
+        (                           )
+        |                           |
+        |   |     |      |      |   |
+        |   |_00__|_01___|__02__|   |
+        |   |     |      |      |   |
+        |   |_10__|_11___|__12__|   |
+        |   |     |      |      |   |
+        |   |_20__|_21___|__22__|   |
+        |                           |
+        (                           )
+        combinet True =    Line
+                        00 - 01 - 02
+                        10 - 11 - 12
+                        20 - 21 -22
+                            Col
+                        00 - 10 - 20
+                        01 - 11 - 21
+                        02 - 12 - 22
+
+                            extra
+                        00 - 11 - 22
+                        20 - 11 - 02
+            only seven combinations are possible
+
+
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        scorePlayer1 = findViewById(R.id.Player1);
+        scorePlayer2 = findViewById(R.id.Player2);
+        equality = findViewById(R.id.Equality);
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                String getButtonsID = "button_" + i + j;
+                int resId = getResources().getIdentifier(getButtonsID, "id", getPackageName());
+                buttons[i][j] = findViewById(resId);
+                buttons[i][j].setOnClickListener(this);
+            }
+        }
+        Button reset = findViewById(R.id.Reset);
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                resetGame();
+            }
+        });
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (!((Button) view).getText().toString().equals("")) {
+            return;
+        }
+        if (firstRound) {
+            ((Button) view).setText("X");
+        } else {
+            ((Button) view).setText("O");
+        }
+        numberOfLaps++;
+        if (checkWhichPlayerWin()) {
+            if (firstRound) {
+                writePlayer1();
+                resetCells();
+
+
+            } else {
+                writePlayer2();
+                resetCells();
+            }
+        } else if (numberOfLaps == 9) {
+            equality();
+            updateEqality();
+            updateScore();
+        } else {
+            firstRound = !firstRound;
+        }
+    }
+
+    public boolean checkWhichPlayerWin() {
+        String[][] combined = new String[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                combined[i][j] = buttons[i][j].getText().toString();
+            }
+            // Check Line
+        }
+        for (int i = 0; i < 3; i++) {
+            if (combined[i][0].equals(combined[i][1]) && combined[i][0].equals(combined[i][2]) && !combined[i][0].equals("")) {
+                return true;
+            }
+        }
+        // Check Col
+        for (int i = 0; i < 3; i++) {
+            if (combined[0][i].equals(combined[1][i]) && combined[0][i].equals(combined[2][i]) && !combined[0][i].equals("")) {
+                return true;
+            }
+        }
+        // Extra main diagonal
+        if (combined[0][0].equals(combined[1][1]) && combined[0][0].equals(combined[2][2]) && !combined[0][0].equals("")) {
+            return true;
+        }
+        if (combined[0][2].equals(combined[1][1]) && combined[0][2].equals(combined[2][0]) && !combined[0][2].equals("")) {
+            return true;
+        }
+
+
+        return false;
+    }
+
+    public void writePlayer1() {
+        playerPuncte1++;
+        updateScore();
+    }
+
+    public void writePlayer2() {
+        playerPuncte2++;
+        updateScore();
+    }
+
+    private void equality() {
+        Toast.makeText(this, "Draw!", Toast.LENGTH_SHORT).show();
+        resetCells();
+    }
+
+    private void resetCells() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                buttons[i][j].setText("");
+            }
+        }
+        numberOfLaps = 0;
+        firstRound = true;
+    }
+
+    private void resetGame() {
+        playerPuncte1 = 0;
+        playerPuncte2 = 0;
+        equalityNumber = 0;
+        updateScore();
+        resetCells();
+    }
+
+    private void updateScore() {
+        scorePlayer1.setText("Player1 : " + playerPuncte1);
+        scorePlayer2.setText("Player2 : " + playerPuncte2);
+        equality.setText("Equality: " + equalityNumber);
+
+
+    }
+
+    private void updateEqality() {
+        equalityNumber++;
+
     }
 }
+
